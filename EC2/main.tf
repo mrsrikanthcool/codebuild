ec950ca1cb947088b12e2c70cfa5214ed3e99586
mrsrikanthcool <97115219+mrsrikanthcool@users.noreply.github.com>
2022-02-23 16:36:19 +0530

    Create main.tf

---
+++ EC2/main.tf
@@ -0,0 +1,13 @@
+provider "aws"{
+	region = "ap-south-1"
+        version = "v2.70.0"
+}
+
+resource "aws_instance" "server"{
+	instance_type = "t2.micro"
+	ami = "ami-0c6615d1e95c98aca"
+   tags{
+     name=codebuild
+  
+  }
+}
